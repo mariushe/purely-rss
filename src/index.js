cd5e18c0c3d76a96e034cb515c2097b172e58ed2
Marius Herring <mariusherring@gmail.com>
2018-03-27 22:23:54 +0200

    Added rss parser and a quick test of the library

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import Cards from './components/Card/cards';
+import RSSParser from 'rss-parser';
 
 class PurelyGhost extends React.Component {
 
     render() {
+
+        const { url } = this.props;
+
+        const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
+        let parser = new RSSParser();
+        parser.parseURL(CORS_PROXY + url + "/rss", function (err, feed) {
+            console.log(feed.title);
+            feed.items.forEach(function (entry) {
+                console.log(entry);
+            })
+        });
 
         return (
             <div>
