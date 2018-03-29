fbd8e61096e3abfff0e532f2e93ae2da74ef290c
Marius Herring <mariusherring@gmail.com>
2018-03-29 17:16:15 +0200

    Cleaning up babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,6 @@ module.exports = {
                 exclude: /(node_modules|bower_components|lib)/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
                 }
             }
         ]
